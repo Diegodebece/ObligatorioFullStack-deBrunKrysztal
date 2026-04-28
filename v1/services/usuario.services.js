a2e7b4e2b47a05e7a312bc64199c4885accf75e1
Diegodebece <diegodebrun@gmail.com>
2026-04-27 22:45:17 -0300

    Cambios FINALISIMOSSS

--- v1/services/usuario.services.js
+++ v1/services/usuario.services.js
@@ -52,7 +52,7 @@ export const cambiarAPlanPremiumService = async (usuarioId, rol) => {
             rol: usuario.rol,
             plan: usuario.plan
         },
-        process.env.JWT_SECRET,
+        process.env.SECRET_KEY,
         {
             expiresIn: "1h"
         }

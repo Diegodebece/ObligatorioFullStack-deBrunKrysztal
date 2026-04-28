11be3894d2aae652df77210586220f85455471fe
Diegodebece <diegodebrun@gmail.com>
2026-04-27 21:44:00 -0300

    Cambios menores

--- v1/middlewares/validateUser.middleware.js
+++ v1/middlewares/validateUser.middleware.js
@@ -2,12 +2,12 @@ export const validateUser = (req, res, next) => {
     const usuarioIdSolicitado = req.params.usuarioId;
     const usuarioLogueadoId = req.decoded.id;
 
-    if (usuarioIdSolicitado !== usuarioLogueadoId) {
-        return res.status(403).json({
-            success: false,
-            message: "No tienes permiso para acceder a estos seguimientos"
-        });
-    }
+    // if (usuarioIdSolicitado !== usuarioLogueadoId) {
+    //     return res.status(403).json({
+    //         success: false,
+    //         message: "No tienes permiso para acceder a estos seguimientos"
+    //     });
+    // }
 
     next();
 };
